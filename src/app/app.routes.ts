1ca5d4667c71c6a6f67425249f05d2d3f9d887d6
L0giXX <marc.mueller04@gmail.com>
2024-04-04 20:42:34 +0200

    add dynamic page for members

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MembersComponent } from './members/members.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
 
 export const routes: Routes = [
   {
@@ -10,11 +11,9 @@ export const routes: Routes = [
   {
     path: 'members',
     component: MembersComponent,
-    children: [
-      {
-        path: 'member/:id',
-        component: MembersComponent,
-      },
-    ],
+  },
+  {
+    path: 'members/:id',
+    component: MemberDetailsComponent,
   },
 ];
---
+++ src/app/members/member-details/member-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './member-details.component.html',
+})
+export class MemberDetailsComponent {}
